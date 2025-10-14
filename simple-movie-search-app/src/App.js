11e659ce6856f9bdc8d219967f104bbee103b30f
Tejas Khile <tejaskhile07@gmail.com>
2025-10-15 00:03:43 +0530

    #309 fix: resolve movie search issue

--- simple-movie-search-app/src/App.js
+++ simple-movie-search-app/src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = process.env.API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -41,7 +41,7 @@ const App = () => {
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
-            <MovieCard movie={movie} />
+            <MovieCard key={movie.imdbID} movie={movie} />
           ))}
         </div>
       ) : (
--- simple-movie-search-app/src/MovieCard.jsx
+++ simple-movie-search-app/src/MovieCard.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
+  
   return (
-    <div className="movie" key={imdbID}>
+    <div className="movie">
       <div>
         <p>{Year}</p>
       </div>
