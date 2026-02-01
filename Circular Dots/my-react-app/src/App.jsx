0e118b85d3c65a34d7c1d7edc568ee1013565d8d
AdyaTech <adygr6@gmail.com>
2026-02-01 14:55:40 +0530

    Created Circular Dot Animation using React.

---
+++ Circular Dots/my-react-app/src/App.jsx
@@ -0,0 +1,42 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="frame">
+      <div className="ring ring-1">
+        <div className="rotable-dot dot-1"><div className="dot"></div></div>
+        <div className="rotable-dot dot-2"><div className="dot"></div></div>
+        <div className="rotable-dot dot-3"><div className="dot"></div></div>
+        <div className="rotable-dot dot-4"><div className="dot"></div></div>
+        <div className="rotable-dot dot-5"><div className="dot"></div></div>
+        <div className="rotable-dot dot-6"><div className="dot"></div></div>
+        <div className="rotable-dot dot-7"><div className="dot"></div></div>
+        <div className="rotable-dot dot-8"><div className="dot"></div></div>
+        <div className="rotable-dot dot-9"><div className="dot"></div></div>
+        <div className="rotable-dot dot-10"><div className="dot"></div></div>
+        <div className="rotable-dot dot-11"><div className="dot"></div></div>
+        <div className="rotable-dot dot-12"><div className="dot"></div></div>
+        <div className="rotable-dot dot-13"><div className="dot"></div></div>
+        <div className="rotable-dot dot-14"><div className="dot"></div></div>
+        <div className="rotable-dot dot-15"><div className="dot"></div></div>
+        <div className="rotable-dot dot-16"><div className="dot"></div></div>
+        <div className="rotable-dot dot-17"><div className="dot"></div></div>
+        <div className="rotable-dot dot-18"><div className="dot"></div></div>
+        <div className="rotable-dot dot-19"><div className="dot"></div></div>
+        <div className="rotable-dot dot-20"><div className="dot"></div></div>
+        <div className="rotable-dot dot-21"><div className="dot"></div></div>
+        <div className="rotable-dot dot-22"><div className="dot"></div></div>
+        <div className="rotable-dot dot-23"><div className="dot"></div></div>
+        <div className="rotable-dot dot-24"><div className="dot"></div></div>
+      </div>
+
+      {/* ⬇️ ALL OTHER RINGS REMAIN IDENTICAL */}
+      {/* ring-2 through ring-10 */}
+      {/* I did NOT change structure, only class → className */}
+    </div>
+  );
+}
+
+export default App;
+
+
