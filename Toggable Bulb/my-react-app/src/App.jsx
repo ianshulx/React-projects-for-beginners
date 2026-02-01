d3adb313c9596d08fe8504ff74864b4595a016b2
AdyaTech <adygr6@gmail.com>
2026-02-01 14:06:46 +0530

    Created a simple Toggable Bulb using React.

---
+++ Toggable Bulb/my-react-app/src/App.jsx
@@ -0,0 +1,23 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="frame">
+      <div className="center">
+        <div className="object">
+          <div className="bulb">
+            <div className="top"></div>
+            <div className="bottom"></div>
+          </div>
+          <div className="screw">
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="end"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
