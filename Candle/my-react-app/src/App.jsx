42366c1a41df88451576df7424691ae262b140fa
AdyaTech <adygr6@gmail.com>
2026-02-02 11:30:12 +0530

    Created a Candle using React.

---
+++ Candle/my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import "./index.css";
+function App() {
+  return (
+    <div className="frame">
+      <div className="candle">
+        <div className="shadow"></div>
+        <div className="wick"></div>
+        <div className="flame"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
