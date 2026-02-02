dda414bec39279501738af446b2e8e171003cd1b
AdyaTech <adygr6@gmail.com>
2026-02-02 11:21:17 +0530

    Created a Sparkling Ruby using React.

---
+++ Sparkle Ruby/my-react-app/src/App.jsx
@@ -0,0 +1,26 @@
+import "./index.css";
+function App() {
+  return (
+    <div className="frame">
+      <div className="ruby">
+        <div className="bottom-left"></div>
+        <div className="bottom-right"></div>
+        <div className="bottom-middle"></div>
+
+        <div className="top-left-left"></div>
+        <div className="top-left"></div>
+        <div className="top-middle"></div>
+
+        <div className="top-right">
+          <div className="flash"></div>
+        </div>
+
+        <div className="top-right-right"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
