4f5a24c1d7848498e72626bcd78dc41dd253a258
AdyaTech <adygr6@gmail.com>
2026-02-04 11:50:05 +0530

    Created a Pyramid using React.

---
+++ Pyramid/my-react-app/src/App.jsx
@@ -0,0 +1,23 @@
+import "./index.css";
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="circle">
+        <div className="sky"></div>
+        <div className="sun"></div>
+        <div className="shadow"></div>
+        <div className="pyramid-left"></div>
+        <div className="pyramid-right"></div>
+        <div className="sand"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
